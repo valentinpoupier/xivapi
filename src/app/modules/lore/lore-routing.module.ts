4dfa25a4a6c1faa47bfc3621b6227b96e11e26cc
Valentin P <poupier_valentin@hotmail.com>
2023-03-30 09:30:53 +0200

    truc

--- src/app/modules/lore/lore-routing.module.ts
+++ src/app/modules/lore/lore-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoreComponent } from './lore/lore.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LoreComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/modules/lore/lore.module.ts
+++ src/app/modules/lore/lore.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { LoreRoutingModule } from './lore-routing.module';
+import { LoreComponent } from './lore/lore.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoreComponent
+  ],
   imports: [
     CommonModule,
     LoreRoutingModule
